3fbcbb9f58052ae3625990dff153c95e2115f912
root <root@localhost.localdomain>
2013-09-07 16:54:05 -0500

    simple_example works

---
+++ SDL_NET_TUTORIAL/simple_sdl_example/client.cpp
@@ -0,0 +1,69 @@
+/*
+	http://www.youtube.com/watch?v=LNSqqxIKX_k
+*/
+
+#include <iostream>
+#include "SDL.h"
+#include "SDL_net.h"
+#include <string.h>
+
+//g++ client.cpp `sdl-config --cflags --libs` -lSDL_image -lSDL_mixer -lSDL_ttf -lSDL_net -o client
+
+const int W = 400;
+const int H = 400;
+
+void game_loop()
+{
+	Surface surface(W, H);
+	Event event;
+	
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	//-----------------------------------------------------------------------------------------
+	// NETWORKING
+	//-----------------------------------------------------------------------------------------
+	SDLNet_Init();
+
+	IPaddress ip;
+	SDLNet_ResolveHost(&ip, "127.0.0.1", 1234); // ip of server, NULL if server ip otherwise, port #
+
+	TCPsocket client = SDLNet_TCP_Open(&ip); // make this a dynamic array later
+
+	char data[100];
+
+	// send request to server to get information
+
+	SDLNet_TCP_Recv(client, data, 100);
+	std::cout << data << std::endl;
+
+
+	//-----------------------------------------------------------------------------------------
+	// GAME LOOP
+	//-----------------------------------------------------------------------------------------
+	while (1)
+	{
+		if (event.poll() && event.type() == QUIT) break;
+
+		surface.lock();
+		surface.put_rect(x, y, w, h, rand() % 256, rand() % 256 , rand() % 256);
+		surface.unlock();
+		surface.flip();
+
+		delay(10);
+	}
+
+
+	SDLNet_TCP_Close(client); // important! if you don't close you cannot connect to the same port again
+	
+	SDLNet_Quit();
+	SDL_Quit();
+	
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	game_loop();
+
+	return 0;
+}
